bf8ac5ecf8e43f48c36e734213109da8725cba54
Antoine Favre <antoinefavre74@gmail.com>
2015-01-22 10:12:56 +0100

    Rajout des quotes avec modif du controleur et de la vue

--- www/js/controllers/QuoteCtrl.js
+++ www/js/controllers/QuoteCtrl.js
@@ -1,14 +1,14 @@
 
 angular.module('app.Quote', [])
 .controller("QuoteCtrl", function($scope, $rootScope, $http){
-	// $rootScope.api_url = api_url_no_slash;
-	// console.log("Quote");
-	// $http.get(api_url+"quotes/api/v1/Quote/?format=json").
-	//   success(function(data) {
-	//   $rootScope.quotes = data;
-	//   console.log(data);
-	//   }).
-	//   error(function(data) {
-	//   console.log(data);
-	//   });
+	$rootScope.api_url = api_url_no_slash;
+	console.log("Quote");
+	$http.get(api_url+"quotes/api/v1/quote/?format=json").
+	  success(function(data) {
+	  $rootScope.quotes = data;
+	  console.log($rootScope.quotes);
+	  }).
+	  error(function(data) {
+	  console.log(data);
+	  });
 });
