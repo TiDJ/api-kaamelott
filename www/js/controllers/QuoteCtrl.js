763423f220ec5801dc8e3a364a13e42080c58c4d
Tom Jamon <tomjamon0726@gmail.com>
2015-01-22 15:30:52 +0100

    V1.0 Pour démonstration

--- www/js/controllers/QuoteCtrl.js
+++ www/js/controllers/QuoteCtrl.js
@@ -3,12 +3,23 @@ angular.module('app.Quote', [])
 .controller("QuoteCtrl", function($scope, $rootScope, $http){
 	$rootScope.api_url = api_url_no_slash;
 	console.log("Quote");
-	$http.get(api_url+"quotes/api/v1/quote/?format=json").
+	$http.get(api_url+"quotes/api/v1/Quote/?format=json").
 	  success(function(data) {
 	  $rootScope.quotes = data;
-	  console.log($rootScope.quotes);
+	  	console.log($rootScope.quotes);
 	  }).
 	  error(function(data) {
-	  console.log(data);
+	  	console.log(data);
 	  });
+
+	$rootScope.toggleGroup = function(group) {
+		if ($rootScope.isGroupShown(group)) {
+			$rootScope.shownGroup = null;
+		} else {
+			$rootScope.shownGroup = group;
+		}
+	};
+	$rootScope.isGroupShown = function(group) {
+		return $rootScope.shownGroup === group;
+	};
 });
